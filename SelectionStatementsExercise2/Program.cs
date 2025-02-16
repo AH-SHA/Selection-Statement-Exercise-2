b163f8c4a0ab9a253c27ddc1009f86aa7988b22f
Shannon Ashford <email.sa.coder@gmail.com>
2025-02-15 23:29:35 -0500

    Tweaked code.

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            
+            Console.WriteLine("What is your favorite school subject?");
+            Console.WriteLine();
+            string favoriteSubject = Console.ReadLine();
+
+            switch (favoriteSubject)
+            {
+                case "Math":
+                    Console.WriteLine("That's mine too!");
+                    break;
+                case "Science":
+                    Console.WriteLine("I'm not so fond of that one.");
+                    break;
+                case "History":
+                    Console.WriteLine("That's a blast from the past!");
+                    break;
+                case "English":
+                    Console.WriteLine("Pero, Usted hable espanol?");
+                    break;
+                case "Geography":
+                    Console.WriteLine("I guess you don't need to use a GPS!");
+                    break;
+                default:
+                    Console.WriteLine($"What do you like about {favoriteSubject}?");
+                    break;
+
+            }
+
+        }     
     }
 }
